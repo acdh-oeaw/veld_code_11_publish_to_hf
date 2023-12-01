849aef052f9d124fe4320b34581633bc697a4dd6
SteffRhes <stefanresch@protonmail.com>
2023-12-01 22:21:34 +0100

    make service more flexible
    - now model name and version can be passed as arguments
    - is supposed to be used in a veld chain
    - spaCy libraries added to the publisher service (as that is sadly
      needed)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM python:3.10.13
 RUN pip install spacy==3.6.0
 RUN pip install spacy-huggingface-hub==0.0.10
-RUN mkdir /app /app/model /app/out
+# sadly, spaCy also needs the base models installed. So they must be downloaded as well. These 
+# pip installs however can also be changed individually
+RUN pip install de_dep_news_trf@https://github.com/explosion/spacy-models/releases/download/de_dep_news_trf-3.5.0/de_dep_news_trf-3.5.0.tar.gz
+RUN mkdir -p /app/ /veld/input/model/ /tmp/out/
 COPY ./run.sh /app/run.sh
 RUN chmod +x /app/run.sh
 WORKDIR /app
--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "#################### building whl with spaCy's package"
-python -m spacy package --force ./model/ ./out --build wheel
+python -m spacy package --force /veld/input/model/ /tmp/out --build wheel --version $version --name $model_name
 
 echo "#################### finding path of produced whl file"
-out_folder=$(ls ./out)
-whl_name=$(ls ./out/"$out_folder"/dist)
-whl_full_path=./out/$out_folder/dist/$whl_name
+out_folder=$(ls /tmp/out)
+whl_name=$(ls /tmp/out/"$out_folder"/dist)
+whl_full_path=/tmp/out/$out_folder/dist/$whl_name
 echo "#################### path of whl file at: ${whl_full_path}"
 
 echo "#################### passing HF token"
